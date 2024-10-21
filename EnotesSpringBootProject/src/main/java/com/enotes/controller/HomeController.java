6d18d4f8d2bc2e628b206ef2494369e653b22d43
admin <admin@DESKTOP-LA9S897>
2024-10-21 14:09:29 +0530

    Added Register page

--- EnotesSpringBootProject/src/main/java/com/enotes/controller/HomeController.java
+++ EnotesSpringBootProject/src/main/java/com/enotes/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.enotes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.enotes.entity.User;
+import com.enotes.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private UserService userService;
 	@GetMapping("/")
 	public String index() 
 	{
@@ -17,6 +27,27 @@ public class HomeController {
 	public String register() 
 	{
 		return "register";
+	}
+	
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute User user, HttpSession session) 
+	{
+		boolean existEmailCheck = userService.existEmailCheck(user.getEmail());
+		
+		if(existEmailCheck) {
+			
+			session.setAttribute("msg", "Email already exist");
+		} else {
+			User saveUser = userService.saveUser(user);
+			if(saveUser!=null) {
+				session.setAttribute("msg", "Register Successful");
+			} else {
+				session.setAttribute("msg", "Something wrong on server");
+			}
+		}
+		
+		
+		return "redirect:/register";
 	}
 	
 	
